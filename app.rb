6224c844ecc804a4819c94ae73d115569a509744
Evan SandersonE <evan.j.sanderson@gmail.coM>
2016-07-26 15:24:21 -0400

    added in making a new pokemon

--- app.rb
+++ app.rb
@@ -19,3 +19,17 @@ get '/pokemon' do
   @pokemon = Pokemon.all
   erb :"pokemon/index"
 end
+
+get '/pokemon/new' do
+  erb :"pokemon/new"
+end
+
+get '/pokemon/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"/pokemon/show"
+end
+
+post '/pokemon' do
+  pokemon = Pokemon.create(params[:pokemon])
+  redirect "/pokemon/#{pokemon.id}"
+end
