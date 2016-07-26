8ed3985d62e0db6f20c8bddabbf868a4d6ca15d7
Evan SandersonE <evan.j.sanderson@gmail.coM>
2016-07-26 15:50:20 -0400

    finished mvp with all CRUD actions

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ end
 
 get '/pokemon' do
   @pokemon = Pokemon.all
-  erb :"pokemon/index"
+  erb :"/pokemon/index"
 end
 
 get '/pokemon/new' do
@@ -32,4 +32,21 @@ end
 post '/pokemon' do
   pokemon = Pokemon.create(params[:pokemon])
   redirect "/pokemon/#{pokemon.id}"
+end
+
+get '/pokemon/:id/edit' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"/pokemon/edit"
+end
+
+put '/pokemon/:id' do
+  pokemon = Pokemon.find(params[:id])
+  pokemon.update(params[:pokemon])
+  redirect "/pokemon/#{pokemon.id}"
+end
+
+delete '/pokemon/:id' do
+  pokemon = Pokemon.find(params[:id])
+  pokemon.destroy
+  redirect '/pokemon'
 end
